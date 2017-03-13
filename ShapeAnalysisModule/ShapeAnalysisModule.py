9fa3df9fdc544c0312db671addcf80890d2f12bf
Laura Pascal <laurajulia.pascal@gmail.com>
2017-03-13 17:18:34 -0400

    Creation of the Interface User

--- ShapeAnalysisModule/ShapeAnalysisModule.py
+++ ShapeAnalysisModule/ShapeAnalysisModule.py
@@ -5,17 +5,17 @@ from slicer.ScriptedLoadableModule import *
 import logging
 
 #
-# SPHARM
+# ShapeAnalysisModule
 #
 
-class SPHARM(ScriptedLoadableModule):
+class ShapeAnalysisModule(ScriptedLoadableModule):
   """Uses ScriptedLoadableModule base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
 
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
-    self.parent.title = "SPHARM"
+    self.parent.title = "Shape Analysis Module"
     self.parent.categories = ["Examples"]
     self.parent.dependencies = []
     self.parent.contributors = ["Laura Pascal (Kitware Inc.)"]
@@ -27,10 +27,10 @@ class SPHARM(ScriptedLoadableModule):
     """ # replace with organization, grant and thanks.
 
 #
-# SPHARMWidget
+# ShapeAnalysisModuleWidget
 #
 
-class SPHARMWidget(ScriptedLoadableModuleWidget):
+class ShapeAnalysisModuleWidget(ScriptedLoadableModuleWidget):
   """Uses ScriptedLoadableModuleWidget base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
@@ -42,7 +42,7 @@ class SPHARMWidget(ScriptedLoadableModuleWidget):
     #  Interface
     #
     loader = qt.QUiLoader()
-    self.moduleName = 'SPHARM'
+    self.moduleName = 'ShapeAnalysisModule'
     scriptedModulesPath = eval('slicer.modules.%s.path' % self.moduleName.lower())
     scriptedModulesPath = os.path.dirname(scriptedModulesPath)
     path = os.path.join(scriptedModulesPath, 'Resources', 'UI', '%s.ui' % self.moduleName)
@@ -53,19 +53,50 @@ class SPHARMWidget(ScriptedLoadableModuleWidget):
     self.widget = widget
     self.layout.addWidget(widget)
 
-
     # Global variables of the Interface
-    self.InputData = self.getWidget('InputDataLineEdit')
+    #   Group Project IO
+    self.GroupProjectInputDirectory = self.getWidget('DirectoryButton_GroupProjectInputDirectory')
+    self.RandomizeInputs = self.getWidget('checkBox_RandomizeInputs')
+    self.GroupProjectOutputDirectory = self.getWidget('DirectoryButton_GroupProjectOutputDirectory')
+    self.Debug = self.getWidget('checkBox_Debug')
+    #   Post Processed Segmentation
+    self.OverwriteSegPostProcess = self.getWidget('checkBox_OverwriteSegPostProcess')
+    self.RescaleSegPostProcess = self.getWidget('checkBox_RescaleSegPostProcess')
+    self.sx = self.getWidget('SliderWidget_sx')
+    self.sy = self.getWidget('SliderWidget_sy')
+    self.sz = self.getWidget('SliderWidget_sz')
+    self.LabelState = self.getWidget('checkBox_LabelState')
+    self.label_ValueLabelNumber = self.getWidget('label_ValueLabelNumber')
+    self.ValueLabelNumber = self.getWidget('SliderWidget_ValueLabelNumber')
+    #   Generate Mesh Parameters
+    self.OverwriteGenParaMesh = self.getWidget('checkBox_OverwriteGenParaMesh')
+    self.NumberofIterations = self.getWidget('SliderWidget_NumberofIterations')
+    #   Parameters to SPHARM Mesh
+    self.OverwriteParaToSPHARMMesh = self.getWidget('checkBox_OverwriteParaToSPHARMMesh')
+    self.SubdivLevelValue = self.getWidget('SliderWidget_SubdivLevelValue')
+    self.SPHARMDegreeValue = self.getWidget('SliderWidget_SPHARMDegreeValue')
+    self.thetaIterationValue = self.getWidget('spinBox_thetaIterationValue')
+    self.phiIterationValue = self.getWidget('spinBox_phiIterationValue')
+    self.medialMesh = self.getWidget('checkBox_medialMesh')
+    #   Visualization
+    #   Apply CLIs
     self.applyButton = self.getWidget('applyButton')
 
     # Connections
+    #   Group Project IO
+    #   Post Processed Segmentation
+    self.LabelState.connect('clicked(bool)', self.onSelectValueLabelNumber)
+    #   Generate Mesh Parameters
+    #   Parameters to SPHARM Mesh
+    #   Visualization
+    #   Apply CLIs
     self.applyButton.connect('clicked(bool)', self.onApplyButton)
 
 
   def cleanup(self):
     pass
 
-    # Functions to recovery the widget in the .ui file
+  # Functions to recovery the widget in the .ui file
   def getWidget(self, objectName):
     return self.findWidget(self.widget, objectName)
 
@@ -79,6 +110,16 @@ class SPHARMWidget(ScriptedLoadableModuleWidget):
           return resulting_widget
     return None
 
+  #
+  #   Post Processed Segmentation
+  #
+  def onSelectValueLabelNumber(self):
+    self.label_ValueLabelNumber.enabled = self.LabelState.checkState()
+    self.ValueLabelNumber.enabled = self.LabelState.checkState()
+
+  #
+  #   Apply CLIs
+  #
   def onApplyButton(self):
     self.callSegPostProcess()
     self.callGenParaMesh()
@@ -87,9 +128,16 @@ class SPHARMWidget(ScriptedLoadableModuleWidget):
   def callSegPostProcess(self):
     #     Creation of the parameters
     parameters = {}
-    slicer.util.loadLabelVolume(self.InputData.currentPath)
-    list = slicer.mrmlScene.GetNodesByClass("vtkMRMLLabelMapVolumeNode")
-    volume = list.GetItemAsObject(0)
+
+    inputList = list()
+    inputDirectory = self.GroupProjectInputDirectory.directory.encode('utf-8')
+    for file in os.listdir(inputDirectory):
+      if file.endswith(".gipl") or file.endswith(".gipl.gz"):
+        filepath = inputDirectory + '/' + file
+        inputList.append(filepath)
+    slicer.util.loadLabelVolume(inputList[0])
+    labelMapVolumeNode_list = slicer.mrmlScene.GetNodesByClass("vtkMRMLLabelMapVolumeNode")
+    volume = labelMapVolumeNode_list.GetItemAsObject(0)
     parameters["fileName"] = slicer.mrmlScene.GetNodesByName(volume.GetName()).GetItemAsObject(0)
 
     output_node = slicer.mrmlScene.AddNode(slicer.vtkMRMLLabelMapVolumeNode())
@@ -115,7 +163,7 @@ class SPHARMWidget(ScriptedLoadableModuleWidget):
     output_surfmesh_model.SetName("output_surfmesh")
     parameters["outSurfName"] = output_surfmesh_model
 
-    parameters["numIterations"] = "200"
+    parameters["numIterations"] = "50"
 
     #     Call the CLI
     print "Call: GenParaMesh"
@@ -130,17 +178,19 @@ class SPHARMWidget(ScriptedLoadableModuleWidget):
     # list = slicer.mrmlScene.GetNodesByClass("vtkMRMLModelNode")
     parameters["inParaFile"] = slicer.mrmlScene.GetNodesByName("output_para").GetItemAsObject(0)
     parameters["inSurfFile"] = slicer.mrmlScene.GetNodesByName("output_surfmesh").GetItemAsObject(0)
-    parameters["outbase"] = slicer.app.temporaryPath + "/OutputSPHARM/test"
+
+    outputDirectory = self.GroupProjectOutputDirectory.directory.encode('utf-8')
+    parameters["outbase"] = outputDirectory + "/test"
     #     Call the CLI
     print "Call: ParaToSPHARMMesh"
     ParaToSPHARMMesh = slicer.modules.paratospharmmeshclp
     slicer.cli.run(ParaToSPHARMMesh, None, parameters, wait_for_completion=True)
 
 #
-# SPHARMLogic
+# ShapeAnalysisModuleLogic
 #
 
-class SPHARMLogic(ScriptedLoadableModuleLogic):
+class ShapeAnalysisModuleLogic(ScriptedLoadableModuleLogic):
   """This class should implement all the actual
   computation done by your module.  The interface
   should be such that other python code can import
@@ -154,7 +204,7 @@ class SPHARMLogic(ScriptedLoadableModuleLogic):
 
 
 
-class SPHARMTest(ScriptedLoadableModuleTest):
+class ShapeAnalysisModuleTest(ScriptedLoadableModuleTest):
   """
   This is the test case for your scripted module.
   Uses ScriptedLoadableModuleTest base class, available at:
