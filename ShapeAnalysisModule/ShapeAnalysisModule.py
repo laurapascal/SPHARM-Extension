33f91dc12a0f88f9abc560cbaccc127fc3b45793
Laura Pascal <laurajulia.pascal@gmail.com>
2017-03-14 16:40:21 -0400

    Creation of a pipeline in the python script to call the three SPHARM CLIs

--- ShapeAnalysisModule/ShapeAnalysisModule.py
+++ ShapeAnalysisModule/ShapeAnalysisModule.py
@@ -39,6 +39,12 @@ class ShapeAnalysisModuleWidget(ScriptedLoadableModuleWidget):
     ScriptedLoadableModuleWidget.setup(self)
 
     #
+    #   Global variables
+    #
+    self.logic = ShapeAnalysisModuleLogic(self)
+    self.pipeline = ShapeAnalysisModulePipeline(self)
+
+    #
     #  Interface
     #
     loader = qt.QUiLoader()
@@ -121,70 +127,8 @@ class ShapeAnalysisModuleWidget(ScriptedLoadableModuleWidget):
   #   Apply CLIs
   #
   def onApplyButton(self):
-    self.callSegPostProcess()
-    self.callGenParaMesh()
-    self.callParaToSPHARMMesh()
-
-  def callSegPostProcess(self):
-    #     Creation of the parameters
-    parameters = {}
-
-    inputList = list()
-    inputDirectory = self.GroupProjectInputDirectory.directory.encode('utf-8')
-    for file in os.listdir(inputDirectory):
-      if file.endswith(".gipl") or file.endswith(".gipl.gz"):
-        filepath = inputDirectory + '/' + file
-        inputList.append(filepath)
-    slicer.util.loadLabelVolume(inputList[0])
-    labelMapVolumeNode_list = slicer.mrmlScene.GetNodesByClass("vtkMRMLLabelMapVolumeNode")
-    volume = labelMapVolumeNode_list.GetItemAsObject(0)
-    parameters["fileName"] = slicer.mrmlScene.GetNodesByName(volume.GetName()).GetItemAsObject(0)
-
-    output_node = slicer.mrmlScene.AddNode(slicer.vtkMRMLLabelMapVolumeNode())
-    output_node.SetName("output_PostProcess")
-    parameters["outfileName"] = output_node.GetID()
-
-    #     Call the CLI
-    print "Call: SegPostProcess"
-    SegPostProcess = slicer.modules.segpostprocessclp
-    slicer.cli.run(SegPostProcess, None, parameters, wait_for_completion=True)
-    slicer.util.saveNode(output_node, slicer.app.temporaryPath + "/outputPostProcess.gipl")
-
-  def callGenParaMesh(self):
-    #     Creation of the parameters
-    parameters = {}
-    parameters["infile"] = slicer.mrmlScene.GetNodesByName("output_PostProcess").GetItemAsObject(0)
-
-    output_para_model = slicer.mrmlScene.AddNode(slicer.vtkMRMLModelNode())
-    output_para_model.SetName("output_para")
-    parameters["outParaName"] = output_para_model
-
-    output_surfmesh_model = slicer.mrmlScene.AddNode(slicer.vtkMRMLModelNode())
-    output_surfmesh_model.SetName("output_surfmesh")
-    parameters["outSurfName"] = output_surfmesh_model
-
-    parameters["numIterations"] = "50"
-
-    #     Call the CLI
-    print "Call: GenParaMesh"
-    GenParaMesh = slicer.modules.genparameshclp
-    slicer.cli.run(GenParaMesh, None, parameters, wait_for_completion=True)
-    slicer.util.saveNode(output_para_model, slicer.app.temporaryPath + "/Parameterization.vtk")
-    slicer.util.saveNode(output_surfmesh_model, slicer.app.temporaryPath + "/SurfaceMesh.vtk")
-
-  def callParaToSPHARMMesh(self):
-    #     Creation of the parameters
-    parameters = {}
-    # list = slicer.mrmlScene.GetNodesByClass("vtkMRMLModelNode")
-    parameters["inParaFile"] = slicer.mrmlScene.GetNodesByName("output_para").GetItemAsObject(0)
-    parameters["inSurfFile"] = slicer.mrmlScene.GetNodesByName("output_surfmesh").GetItemAsObject(0)
-
-    outputDirectory = self.GroupProjectOutputDirectory.directory.encode('utf-8')
-    parameters["outbase"] = outputDirectory + "/test"
-    #     Call the CLI
-    print "Call: ParaToSPHARMMesh"
-    ParaToSPHARMMesh = slicer.modules.paratospharmmeshclp
-    slicer.cli.run(ParaToSPHARMMesh, None, parameters, wait_for_completion=True)
+    self.pipeline.setup()
+    self.pipeline.runNextCLIModule()
 
 #
 # ShapeAnalysisModuleLogic
@@ -199,10 +143,117 @@ class ShapeAnalysisModuleLogic(ScriptedLoadableModuleLogic):
   Uses ScriptedLoadableModuleLogic base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
-  pass
+  def __init__(self, interface):
+    self.interface = interface
 
+#
+# ShapeAnalysisModulePipeline
+#
+class ShapeAnalysisModulePipeline():
+  def __init__(self, interface):
+    self.interface = interface
 
+    # Modules
+    self.ID = -1
+    self.slicerModule = {}  # queue of the modules
+    self.moduleParameters = {}
+    self.output = {}
+    self.output_path = {}
+    self.saveOutput = {}
 
+  def setupModule(self, module, cli_parameters, cli_output, cli_path_output, saveOutput):
+
+    self.slicerModule[self.ID] = module
+    self.moduleParameters[self.ID] = cli_parameters
+    self.output[self.ID] = cli_output
+    self.output_path[self.ID] = cli_path_output
+    self.saveOutput[self.ID] = saveOutput
+
+  def setup(self):
+
+    # SegPostProcess
+    self.ID = 0
+
+    cli_parameters = {}
+    inputList = list()
+    inputDirectory = self.interface.GroupProjectInputDirectory.directory.encode('utf-8')
+    for file in os.listdir(inputDirectory):
+      if file.endswith(".gipl") or file.endswith(".gipl.gz"):
+        filepath = inputDirectory + '/' + file
+        inputList.append(filepath)
+    slicer.util.loadLabelVolume(inputList[0])
+    labelMapVolumeNode_list = slicer.mrmlScene.GetNodesByClass("vtkMRMLLabelMapVolumeNode")
+    volume = labelMapVolumeNode_list.GetItemAsObject(0)
+    cli_parameters["fileName"] = slicer.mrmlScene.GetNodesByName(volume.GetName()).GetItemAsObject(0)
+
+    output_node = slicer.mrmlScene.AddNode(slicer.vtkMRMLLabelMapVolumeNode())
+    output_node.SetName("output_PostProcess")
+    cli_parameters["outfileName"] = output_node.GetID()
+
+    cli_output = list()
+    cli_output.append(output_node)
+    cli_output_path = list()
+    cli_output_path.append(slicer.app.temporaryPath + "/outputPostProcess.gipl")
+
+    self.setupModule(slicer.modules.segpostprocessclp, cli_parameters, cli_output, cli_output_path, True)
+
+    # GenParaMesh
+    self.ID += 1
+
+    cli_parameters = {}
+    cli_parameters["infile"] = output_node
+
+    output_para_model = slicer.mrmlScene.AddNode(slicer.vtkMRMLModelNode())
+    output_para_model.SetName("output_para")
+    cli_parameters["outParaName"] = output_para_model
+
+    output_surfmesh_model = slicer.mrmlScene.AddNode(slicer.vtkMRMLModelNode())
+    output_surfmesh_model.SetName("output_surfmesh")
+    cli_parameters["outSurfName"] = output_surfmesh_model
+
+    cli_parameters["numIterations"] = "50"
+
+    cli_output = list()
+    cli_output.append(output_para_model)
+    cli_output.append(output_surfmesh_model)
+    cli_output_path = list()
+    cli_output_path.append(slicer.app.temporaryPath + "/Parameterization.vtk")
+    cli_output_path.append(slicer.app.temporaryPath + "/SurfaceMesh.vtk")
+
+    self.setupModule(slicer.modules.genparameshclp, cli_parameters, cli_output, cli_output_path, True)
+
+    # ParaToSPHARMMesh
+    self.ID += 1
+
+    cli_parameters = {}
+
+    cli_parameters["inParaFile"] = output_para_model
+
+    cli_parameters["inSurfFile"] = output_surfmesh_model
+
+    outputDirectory = self.interface.GroupProjectOutputDirectory.directory.encode('utf-8')
+    cli_parameters["outbase"] = outputDirectory + "/test"
+
+    cli_output = list()
+    cli_output_path = list()
+
+    self.setupModule(slicer.modules.paratospharmmeshclp, cli_parameters, cli_output, cli_output_path, False)
+
+  def runCLIModule(self):
+    slicer.cli.run(self.slicerModule[self.ID], None, self.moduleParameters[self.ID], wait_for_completion=True)
+
+  def saveOutputs(self):
+    cli_output = self.output[self.ID]
+    cli_output_path = self.output_path[self.ID]
+    for i in range(0, len(cli_output)):
+      slicer.util.saveNode(cli_output[i], cli_output_path[i])
+
+  def runNextCLIModule(self):
+    for i in range(0, 3):
+      self.ID = i
+      self.runCLIModule()
+      if self.saveOutput[self.ID]:
+        self.saveOutputs()
 
 class ShapeAnalysisModuleTest(ScriptedLoadableModuleTest):
   """
